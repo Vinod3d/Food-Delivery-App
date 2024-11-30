72cff24989ce17ee35245a92252ac2c2f352b1f6
Vinod Chandra <vinodchandra979@gmail.com>
2024-11-30 18:08:54 +0530

    added redux persist to store user data in localstorage

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,11 +2,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
-import store from './store/store.js'
+import { PersistGate } from 'redux-persist/integration/react';
+import store, { persistor } from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
   
 )
--- Client/src/store/store.js
+++ Client/src/store/store.js
@@ -1,18 +1,30 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; 
+import { combineReducers } from 'redux';
 import imageReducer from './slices/imageSlice.js'
 import userReducer from './slices/userSlice.js'
 import offerReducer from './slices/offerSlice.js'
 import categoryReducer from './slices/categorySlice.js'
 import restaurantReducer from './slices/restaurantSlice.js'
 
+const userPersistConfig = {
+    key: 'user',
+    storage,
+};
+
+const rootReducer = combineReducers({
+    image: imageReducer,
+    user: persistReducer(userPersistConfig, userReducer),
+    offer: offerReducer,
+    category: categoryReducer,
+    restaurant: restaurantReducer,
+});
+
 const store = configureStore({
-    reducer: {
-        image: imageReducer,
-        user: userReducer,
-        offer: offerReducer,
-        category: categoryReducer,
-        restaurant: restaurantReducer
-    },
-})
+    reducer: rootReducer,
+});
+
+export const persistor = persistStore(store);
 
 export default store;
